42f45b2e68ccc20f4e17cf745f4c1106ada411d7
vera <aus2.vera@gmail.com>
2015-04-16 01:23:10 +1000

    border will only be shown for movable piece

--- src/controller/PieceMovementListener.java
+++ src/controller/PieceMovementListener.java
@@ -13,6 +13,8 @@ import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
+import model.AbstractPiece;
+import model.Barrier;
 import model.GameManager;
 import model.PieceGroup;
 import view.BoardPanel;
@@ -33,10 +35,12 @@ public class PieceMovementListener implements MouseListener {
 
 	private SquarePanel currSquares[][];
 	private SquarePanel oldSquares[][];
-	
+
+	private PieceGroup newSquareArray;
+
 	private static SquarePanel prevSP = null;
 	private static ArrayList<SquarePanel> prevSPList = null;
-	
+
 	public static SquarePanel selectedPieceSquarePanel = null;
 
 	public PieceMovementListener(SquarePanel psp) {
@@ -44,52 +48,57 @@ public class PieceMovementListener implements MouseListener {
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {
+	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {
+	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 
-		if(prevSPList == null){
+		if (prevSPList == null) {
 			prevSPList = new ArrayList<SquarePanel>();
-		}
-		else{
-			for(int c=0;c<prevSPList.size();c++){
-				prevSPList.get(c).setBorder(BorderFactory
-						.createLineBorder(Color.GREEN, 0));
+		} else {
+			for (int c = 0; c < prevSPList.size(); c++) {
+				prevSPList.get(c).setBorder(
+						BorderFactory.createLineBorder(Color.GREEN, 0));
 			}
-			
+
 		}
 
 		// TODO Auto-generated method stub
 		if (this.parentSquarePanel.getCurrentPieceGroup() != null) {
 			PieceMovementListener.selectedPieceSquarePanel = this.parentSquarePanel;
 
-			// need to check if the piece is movable
-			// piece!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-			// show border if player click a square
-			if(prevSP != null){
-				prevSP.setBorder(BorderFactory.createLineBorder(
-						Color.BLUE, 0));
+			if (prevSP != null) {
+				prevSP.setBorder(BorderFactory.createLineBorder(Color.BLUE, 0));
 			}
-			parentSquarePanel.setBorder(BorderFactory.createLineBorder(
-					Color.BLUE, 3));
+
 			prevSP = parentSquarePanel;
 
 			currX = this.parentSquarePanel.getGridLocation().x;
 			currY = this.parentSquarePanel.getGridLocation().y;
-			
+
+			newSquareArray = GameManager.getSingleton().getBoard()
+					.getPiece(currY, currX);
+
+			//only show border for movable piece
+			AbstractPiece piece = newSquareArray.getPieces().get(0);
+			if (!(piece instanceof Barrier)) {
+				parentSquarePanel.setBorder(BorderFactory.createLineBorder(
+						Color.BLUE, 3));
+			}
+
 			View v = View.getView();
 			currSquares = v.getBoardPanel().getSquares();
-			
+
 			point = GameManager.getSingleton().getBoard()
 					.getValidMoves(currY, currX);
 
-
-			//Remove after demo
-			try{
+			// Remove after demo
+			try {
 				for (int i = 0; i < currSquares.length; i++) {
 					for (int j = 0; j < currSquares.length; j++) {
 						for (int m = 0; m < point.size(); m++) {
@@ -97,21 +106,19 @@ public class PieceMovementListener implements MouseListener {
 									point.get(m))) {
 								currSquares[j][i].setBorder(BorderFactory
 										.createLineBorder(Color.GREEN, 3));
-								
+
 								prevSPList.add(currSquares[j][i]);
 
 							}
 						}
-						
+
 					}
 				}
-			}
-			catch(Exception e1){
+			} catch (Exception e1) {
 				System.out.println("Cannot move barrier");
 			}
-			
-		} 
-		else {
+
+		} else {
 			if (PieceMovementListener.selectedPieceSquarePanel != null) {
 
 				// move from
@@ -123,34 +130,30 @@ public class PieceMovementListener implements MouseListener {
 				oldY = from.y;
 				View v = View.getView();
 				oldSquares = v.getBoardPanel().getSquares();
-				pointA = GameManager.getSingleton().getBoard().getValidMoves(oldY, oldX);
+				pointA = GameManager.getSingleton().getBoard()
+						.getValidMoves(oldY, oldX);
 
 				for (int x = 0; x < pointA.size(); x++) {
 					if (to.x == pointA.get(x).y && to.y == pointA.get(x).x) {
-						GameManager.getSingleton().getBoard().movePieces(from, to);
+						GameManager.getSingleton().getBoard()
+								.movePieces(from, to);
 					}
 				}
-				
+
 				// change back to no border -- show selected piece
 				selectedPieceSquarePanel.setBorder(BorderFactory
 						.createLineBorder(Color.BLUE, 0));
-				
-				v.getInfo().setTimer(); 
+
+				v.getInfo().setTimer();
 				PieceMovementListener.selectedPieceSquarePanel = null;
 			}
 		}
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mouseEntered(MouseEvent e) {}
 
 	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mouseExited(MouseEvent e) {}
 
 }
